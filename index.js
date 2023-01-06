0d0579321e31339680723f65596cbf46904551e7
animekkk <animekgaming@gmail.com>
2023-01-06 21:37:41 +0100

    Fixing some bugs and adding star sort system

--- index.js
+++ index.js
@@ -2,8 +2,18 @@ const STEAM_LINK = 'https://steamcommunity.com/profiles/%decId%';
 
 let refreshTime = 31;
 let refreshInterval;
+let starsIds = [];
+
+let lastId;
+let lastPlayers = [];
 
 window.onload = () => {
+	const stars = localStorage.getItem('stars');
+	if (stars) {
+		starsIds.push(...stars.split(';'));
+		console.info(`Loaded ${starsIds.length}`);
+	}
+
 	const lastId = localStorage.getItem('lastId');
 	if (!lastId) return;
 
@@ -14,13 +24,15 @@ window.onload = () => {
 const handleInput = () => {
 	const value = document.querySelector('#input-id').value;
 	if (!value) return;
+
+	updateInfo(value);
 };
 
 const updateInfo = (serverId) => {
 	console.info('Getting info from', serverId);
 	localStorage.setItem('lastId', serverId);
+	lastId = serverId;
 
-	resetTable();
 	fetch(`https://servers-frontend.fivem.net/api/servers/single/${serverId}`)
 		.then((response) => response.json())
 		.then((json) => {
@@ -28,44 +40,88 @@ const updateInfo = (serverId) => {
 
 			document.querySelector('#hostname').textContent = `Players ${json.Data.clients}/${json.Data.sv_maxclients}`;
 
-			const table = document.querySelector('table');
-			json.Data.players.forEach((player) => {
-				const tr = document.createElement('tr');
-
-				const id = document.createElement('th');
-				const name = document.createElement('th');
-				const steam = document.createElement('th');
-				const ping = document.createElement('th');
-
-				id.textContent = player.id;
-				name.textContent = player.name;
-				steam.textContent = 'no';
-				ping.textContent = player.ping;
-
-				tr.appendChild(id);
-				tr.appendChild(name);
-				tr.appendChild(steam);
-				tr.appendChild(ping);
-
-				const filteredIdentifiers = player.identifiers.filter((identifier) => identifier.startsWith('steam:'));
-				if (filteredIdentifiers.length > 0) {
-					const steamId = hexToDecimal(filteredIdentifiers[0].substring(filteredIdentifiers[0].indexOf(':') + 1));
-
-					const steamURL = STEAM_LINK.replace('%decId%', steamId);
-					const steamLink = document.createElement('a');
-					steamLink.href = steamURL;
-					steamLink.textContent = 'yes';
-
-					steam.textContent = '';
-					steam.appendChild(steamLink);
-				}
-
-				table.appendChild(tr);
-
-				updateRefresh(serverId);
-			});
+			lastPlayers = json.Data.players;
+			displayPlayers(lastPlayers, true);
 		})
 		.catch((error) => console.error(error));
+};
+
+const displayPlayers = (players, refresh) => {
+	const table = document.querySelector('table');
+
+	players.forEach((player) => setSteamId(player));
+	players.sort((x, y) => {
+		return Number(starsIds.includes(y.steamId)) - Number(starsIds.includes(x.steamId));
+	});
+
+	resetTable();
+	players.forEach((player) => {
+		const tr = document.createElement('tr');
+
+		// Star Start
+		const top = document.createElement('th');
+		const topText = document.createElement('span');
+		topText.textContent = '-';
+		topText.title = `This player doesn't have connected Steam account, so it won't work.`;
+		if (player.steamId) {
+			topText.className = 'star';
+			topText.addEventListener('click', () => {
+				const arrayId = starsIds.indexOf(player.steamId);
+				if (arrayId !== -1) starsIds.splice(arrayId, 1);
+				else starsIds.push(player.steamId);
+
+				localStorage.setItem('stars', starsIds.join(';'));
+				displayPlayers(lastPlayers, false);
+			});
+			if (starsIds.includes(player.steamId)) {
+				topText.textContent = '⭐';
+				topText.title = `Click here to don't show this player on top of the table.`;
+			} else {
+				topText.textContent = '☆';
+				topText.title = `Click here to show this player on top of the table.`;
+			}
+		}
+
+		top.appendChild(topText);
+
+		// Star End
+
+		const id = document.createElement('th');
+		const name = document.createElement('th');
+		const steam = document.createElement('th');
+		const ping = document.createElement('th');
+
+		id.textContent = player.id;
+		name.textContent = player.name;
+		steam.textContent = 'no';
+		ping.textContent = `${player.ping}ms`;
+
+		if (player.steamId) {
+			const steamURL = STEAM_LINK.replace('%decId%', player.steamId);
+			const steamLink = document.createElement('a');
+			steamLink.href = steamURL;
+			steamLink.textContent = 'yes';
+
+			steam.textContent = '';
+			steam.appendChild(steamLink);
+		}
+
+		tr.appendChild(top);
+		tr.appendChild(id);
+		tr.appendChild(name);
+		tr.appendChild(steam);
+		tr.appendChild(ping);
+
+		table.appendChild(tr);
+	});
+	if (refresh) updateRefresh(lastId);
+};
+
+const setSteamId = (player) => {
+	const filteredIdentifiers = player.identifiers.filter((identifier) => identifier.startsWith('steam:'));
+	if (filteredIdentifiers.length > 0) {
+		player.steamId = hexToDecimal(filteredIdentifiers[0].substring(filteredIdentifiers[0].indexOf(':') + 1));
+	} else player.steamId = false;
 };
 
 const resetTable = () => {
